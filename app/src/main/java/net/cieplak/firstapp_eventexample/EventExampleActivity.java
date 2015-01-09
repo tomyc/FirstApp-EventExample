58a9a8f6024b2ad9bb5ed1d14cba09149bc4b504
tomyc <tomasz@cieplak.net>
2015-01-09 17:01:00 +0100

    Przykład nr 6

---
+++ app/src/main/java/net/cieplak/firstapp_eventexample/EventExampleActivity.java
@@ -0,0 +1,57 @@
+package net.cieplak.firstapp_eventexample;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class EventExampleActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_example);
+        Button button = (Button) findViewById(R.id.myButton);
+        button.setOnClickListener(
+                new Button.OnClickListener(){public void onClick(View v){
+                    TextView myTextView = (TextView) findViewById(R.id.myTextView);
+                    myTextView.setText(getString(R.string.button_click));
+                }
+        });
+
+        button.setOnLongClickListener(
+                new Button.OnLongClickListener() {public boolean onLongClick(View v){
+                    TextView myTextView = (TextView) findViewById(R.id.myTextView);
+                    myTextView.setText("Long button click");
+                    return true;
+                }}
+        );
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_event_example, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
